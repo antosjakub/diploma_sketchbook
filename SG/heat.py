c2947e23d3b3f501b609e631ac1439fb0dab4e61
Jakub Antos <antos_j@outlook.com>
2026-03-23 20:04:15 +0100

    SG: run dim=7 on simple heat eq.

--- SG/heat.py
+++ SG/heat.py
@@ -1,21 +1,29 @@
+
+###############################################################################
+################## SPARSE GRIDS ###############################################
+###############################################################################
 import pysgpp
-import numpy as np
+
 
 # 1. Initialize Grid (Zero Dirichlet boundaries)
-dim = 2
+dim = 7
 level = 5
 grid = pysgpp.Grid.createLinearGrid(dim)
 grid.getGenerator().regular(level)
 gridStorage = grid.getStorage()
 
+
 # 2. Set Initial Condition u(x,y,0) = sin(pi*x)*sin(pi*y)
+import numpy as np
 node_values = pysgpp.DataVector(gridStorage.getSize())
 for i in range(gridStorage.getSize()):
     pt = gridStorage.getPoint(i)
-    x, y = pt.getStandardCoordinate(0), pt.getStandardCoordinate(1)
-    node_values[i] = np.sin(np.pi * x) * np.sin(np.pi * y)
+    node_values[i] = 1.0
+    for di in range(dim):
+        node_values[i] *= np.sin(np.pi * pt.getStandardCoordinate(di))
 
 alpha = pysgpp.DataVector(node_values)
+print(f"len(alpha) = {len(alpha)}")
 pysgpp.createOperationHierarchisation(grid).doHierarchisation(alpha)
 
 # 3. Setup PDE Operators
@@ -27,15 +35,18 @@ dt = 0.001
 steps = 100
 diffusivity = 0.1
 
+print("Starting calculation...")
 for step in range(steps):
     opLaplace.mult(alpha, laplace_res)
     laplace_res.mult(dt * diffusivity)
     alpha.sub(laplace_res) # Approximation omitting Mass Matrix inversion
-print(len(alpha))
+print("Calculation done.")
 
 
-import numpy as np
-import matplotlib.pyplot as plt
+
+###############################################################################
+################## PLOT RESULTS ###############################################
+###############################################################################
 
 # --- Resolution of the plot ---
 N = 100
@@ -49,10 +60,11 @@ Z = np.zeros((N, N))
 
 for i in range(N):
     for j in range(N):
-        pt = pysgpp.DataVector([X[i, j], Y[i, j]])
+        pt = pysgpp.DataVector([X[i, j], Y[i, j]] + (dim-2)*[0.23])
         Z[i, j] = op_eval.eval(alpha, pt)
 
 # --- Plot ---
+import matplotlib.pyplot as plt
 fig, axes = plt.subplots(1, 2, figsize=(12, 5))
 
 # Heatmap
